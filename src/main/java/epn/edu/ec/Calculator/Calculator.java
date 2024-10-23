bdd503aabc8961c06a4d8de82e1ff65daf6c49ea
Kevin Sebastian D <kedose@outlook.it>
2024-10-22 22:44:59 -0500

    Adding calculator class and unit tests.

---
+++ src/main/java/epn/edu/ec/Calculator/Calculator.java
@@ -0,0 +1,34 @@
+package epn.edu.ec.Calculator;
+
+public class Calculator {
+    private int answer;
+
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
+    public double division(int a, int b) {
+        return a / b;
+    }
+
+    public void timeout(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+}
